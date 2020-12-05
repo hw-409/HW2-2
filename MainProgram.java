e6090398c5987a292788e35a63006276f49c021e
URI <spqjf12345@gmail.com>
2020-12-05 16:57:35 +0900

    updated

--- MainProgram.java
+++ MainProgram.java
@@ -30,9 +30,32 @@ public class MainProgram {
         	System.out.println("[Student ID: 1710344]");
         	hw02_1710344();
         }
-        else
+        else if (studentID == 1711905) {
+    			int num;
+    			do {
+    				num = calBanner_1711905();
+    				if(num == 1) {
+    					int arr[] = makeArray_1711905();
+    					System.out.println("Max number is " + max_1711905(arr));
+    					System.out.println();
+    					break;
+    				}
+    				else if(num ==2){
+    					int arr[] = makeArray_1711905();
+    					System.out.println("Min number is " + min_1711905(arr));
+    					System.out.println();
+    					break;
+    				}
+    				else {
+    					System.out.println("Please enter 1 or 2");
+    				}
+    			} while((num != 1) || (num != 2));
+    	}
+        
+        else 
             System.out.println("To be developed...\n");
     }
+
     
     public void hw02_1710344() {
     	int num1;
@@ -69,5 +92,44 @@ public class MainProgram {
     	else return b;
     }
     
+    
+    public int calBanner_1711905() {
+		int number;
+		System.out.println("[Student ID: 1711905]");
+		System.out.println("1. Calculate the Max value");
+		System.out.println("2. Calculate the Min value");
+		System.out.print("Enter menu number : ");
+		Scanner input = new Scanner(System.in);
+		number = input.nextInt();
+		return number;
+	}
+    
+    public int[] makeArray_1711905() {
+		int[] arr = new int[5];
+		System.out.println("Enter the number 5 times : ");
+		for(int i =0; i<5; i++) {
+			Scanner input = new Scanner(System.in);
+			arr[i] = input.nextInt();
+		}
+		return arr;
+	}
+    
+    public int max_1711905(int[] arr) {
+		int max = arr[0];
+		for(int i=1; i<arr.length; i++) {
+			if(max < arr[i])
+				max = arr[i];
+		}
+		return max;	
+	}
+	
+	public int min_1711905(int[] arr) {
+		int min = arr[0];
+		for(int i=1; i<arr.length; i++) {
+			if(min > arr[i])
+				min = arr[i];
+		}
+		return min;	
+	}   
     
 }
